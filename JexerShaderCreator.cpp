891803009c11ef019dda3390a93b417393822636
WorkingXXX <dx15622133362@gmail.com>
2018-09-17 10:19:08 +0800

    Add JexerShaderCreator

---
+++ JexerShaderCreator.cpp
@@ -0,0 +1,2 @@
+
+#include "JexerShaderManager.h"
---
+++ JexerShaderCreator.h
@@ -0,0 +1,35 @@
+//
+// Author : DX, 2018/9/17
+//
+
+#pragma once
+
+#ifndef JEXER_SHADER_CREATOR_H
+#define JEXER_SHADER_CREATOR_H
+
+namespace Jexer
+{
+	/**
+		
+	*/
+	class JexerShaderCreator
+	{
+	public:
+
+		JexerShaderCreator();
+
+		~JexerShaderCreator();
+
+		// ªÒ»°Shader
+		void GainShader();
+
+	protected:
+
+	private:
+
+	};
+
+
+} // Jexer
+
+#endif // JEXER_SHADER_CREATOR_H
